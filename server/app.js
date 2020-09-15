fcd87c6c78cb4743bfd99a9c79595902d3be9329
Zahra Kirschblüte <zahra.kirschblute@hotmail.com>
2020-09-15 19:28:01 +0200

    Enable user to sign up server-side via the route /api/signup

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
-
 // import routes
 const userRoutes = require('./routes/user');
 
@@ -15,6 +17,11 @@ mongoose
     useCreateIndex: true,
   })
   .then(() => console.log('DB Connected'));
+
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 // routes middleware
 app.use('/api', userRoutes);
