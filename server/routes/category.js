4d3dc38651d8efbb8b0049ee40f113d5672d47ab
Zahra Kirschblüte <zahra.kirschblute@hotmail.com>
2020-09-21 20:15:21 +0200

    Create product model

--- server/routes/category.js
+++ server/routes/category.js
@@ -4,6 +4,12 @@ const router = express.Router();
 
 const { create } = require('../controllers/category');
 
-router.post('/category/create', create);
+const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
+
+const { userById } = require('../controllers/user');
+
+router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create);
+
+router.param('userId', userById);
 
 module.exports = router;
