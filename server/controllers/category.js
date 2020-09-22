010e0fd31df4dcf2bd1ca39f93bd66c2dfe151b6
Zahra Kirschblüte <zahra.kirschblute@hotmail.com>
2020-09-22 11:15:24 +0200

    Create update & delete for categories #5

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,61 +1,69 @@
-const formidable = require('formidable');
-
-const _ = require('lodash');
-
-const fs = require('fs');
-
-const Product = require('../models/product');
+const Category = require('../models/category');
 
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
+exports.categoryById = (req, res, next, id) => {
+  Category.findById(id).exec((err, category) => {
+    if (err || !category) {
+      return res.status(400).json({
+        error: 'Category does not exist',
+      });
+    }
+    req.category = category;
+    next();
+  });
+};
+
 exports.create = (req, res) => {
-  let form = new formidable.IncomingForm();
-  form.keepExtensions = true;
-  form.parse(req, (err, fields, files) => {
+  const category = new Category(req.body);
+  category.save((err, data) => {
     if (err) {
       return res.status(400).json({
-        error: 'Image could not be uploaded',
+        error: errorHandler(err),
       });
     }
-    // check for all fields
-    const { name, description, price, category, quantity, shipping } = fields;
+    res.json({ data });
+  });
+};
 
-    if (
-      !name ||
-      !description ||
-      !price ||
-      !category ||
-      !quantity ||
-      !shipping
-    ) {
+exports.read = (req, res) => {
+  return res.json(req.category);
+};
+
+exports.update = (req, res) => {
+  const category = req.category;
+  category.name = req.body.name;
+  category.save((err, data) => {
+    if (err) {
       return res.status(400).json({
-        error: 'All fields are required',
+        error: errorHandler(err),
       });
     }
+    res.json(data);
+  });
+};
 
-    let product = new Product(fields);
-
-    // 1kb = 1000
-    // 1mb = 1000000
-
-    if (files.photo) {
-      // console.log("FILES PHOTO: ", files.photo);
-      if (files.photo.size > 1000000) {
-        return res.status(400).json({
-          error: 'Image should be less than 1mb in size',
-        });
-      }
-      product.photo.data = fs.readFileSync(files.photo.path);
-      product.photo.contentType = files.photo.type;
+exports.remove = (req, res) => {
+  const category = req.category;
+  category.remove((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
     }
-
-    product.save((err, result) => {
-      if (err) {
-        return res.status(400).json({
-          error: errorHandler(err),
-        });
-      }
-      res.json(result);
+    res.json({
+      message: 'Category deleted',
     });
+  });
+};
+
+exports.list = (req, res) => {
+  Category.find().exec((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json(data);
   });
 };
