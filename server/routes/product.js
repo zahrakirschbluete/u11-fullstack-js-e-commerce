2a4d93e570e3f9ca7e0135117043fe78f3972ee1
Zahra Kirschblüte <zahra.kirschblute@hotmail.com>
2020-09-22 11:56:36 +0200

    List products by search #6

--- server/routes/product.js
+++ server/routes/product.js
@@ -11,6 +11,7 @@ const {
   list,
   listRelated,
   listCategories,
+  listBySearch,
 } = require('../controllers/product');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
@@ -35,6 +36,7 @@ router.put(
 router.get('/products', list);
 router.get('/products/related/:productId', listRelated);
 router.get('/products/categories', listCategories);
+router.post('/products/by/search', listBySearch);
 
 router.param('userId', userById);
 router.param('productId', productById);
