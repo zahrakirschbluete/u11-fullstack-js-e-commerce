2f9aab6102bf3d8a44c88f6d0a338019766d9eda
Zahra Kirschblüte <zahra.kirschblute@hotmail.com>
2020-09-28 13:07:52 +0200

    Display categories in shop page with checkboxes

--- client/src/core/Shop.js
+++ client/src/core/Shop.js
@@ -1,8 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
 import Card from './Card';
+import { getCategories } from './apiCore';
+import Checkbox from './Checkbox';
 
 const Shop = () => {
+  const [categories, setCategories] = useState([]);
+  const [error, setError] = useState(false);
+
+  const init = () => {
+    getCategories().then(data => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setCategories(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  const handleFilters = (filters, filterBy) => {
+    console.log('SHOP', filters, filterBy);
+  };
+
   return (
     <Layout
       title='Shop Page'
@@ -10,7 +33,15 @@ const Shop = () => {
       className='container-fluid'
     >
       <div className='row'>
-        <div className='col-4'>left sidebar</div>
+        <div className='col-4'>
+          <h4>Filter by categories</h4>
+          <ul>
+            <Checkbox
+              categories={categories}
+              handleFilters={filters => handleFilters(filters, 'category')}
+            />
+          </ul>
+        </div>
 
         <div className='col-8'>right</div>
       </div>
