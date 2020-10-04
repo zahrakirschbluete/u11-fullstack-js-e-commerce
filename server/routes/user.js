fcf9b244b019eab8da878b3b89defeb05dac81ad
Zahra Kirschblüte <zahra.kirschblute@hotmail.com>
2020-10-04 19:42:44 +0200

    User purchase history method server side

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,14 @@
 const express = require('express');
-
 const router = express.Router();
 
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 
-const { userById, read, update } = require('../controllers/user');
+const {
+  userById,
+  read,
+  update,
+  purchaseHistory,
+} = require('../controllers/user');
 
 router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
   res.json({
@@ -14,6 +18,7 @@ router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
 
 router.get('/user/:userId', requireSignin, isAuth, read);
 router.put('/user/:userId', requireSignin, isAuth, update);
+router.get('/orders/by/user/:userId', requireSignin, isAuth, purchaseHistory);
 
 router.param('userId', userById);
 
