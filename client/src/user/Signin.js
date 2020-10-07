a88f5ecdae4c6781ca86d4f1473e37208a65041e
Zahra Kirschblüte <zahra.kirschblute@hotmail.com>
2020-10-07 20:59:02 +0200

    Remove user info from signin.js

--- client/src/user/Signin.js
+++ client/src/user/Signin.js
@@ -5,8 +5,8 @@ import { signin, authenticate, isAuthenticated } from '../auth';
 
 const Signin = () => {
   const [values, setValues] = useState({
-    email: 'scrappy@live.se',
-    password: 'qwerty1',
+    email: '',
+    password: '',
     error: '',
     loading: false,
     redirectToReferrer: false,
